e12099a3521b2609a105a03204d6bba28d669439
Matthieu96Code <senoumatthieu96@gmail.com>
2023-12-25 12:30:03 +0000

    Import user deatils on user and create static data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+// import { UserDetailsComponent } from './user-details/user-details.component';
 
 @Component({
   selector: 'app-root',
+  // standalone: true,
+  // imports: [UserDetailsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'gdg-project-angular16';
+  users: User[] = [
+    {
+      id: 'a2bf',
+      name: 'RaKNj',
+      sex: 'M'
+    },
+    {
+      id: 'a2aa',
+      name: 'AliK',
+      sex: 'M'
+    },
+    {
+      id: 'a19f',
+      name: 'Ell@d',
+      sex: 'F'
+    }
+  ]
+
+  currenttUser: User = this.users[1];
 }
